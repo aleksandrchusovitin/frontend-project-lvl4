beb9560e8c3a83edec3c955f56906f60993cafd9
Aleksandr Chusovitin <chusovitinalexandr@gmail.com>
2021-11-14 13:14:29 +0700

    fix boilerplate

--- src/index.js
+++ src/index.js
@@ -16,26 +16,5 @@ if (process.env.NODE_ENV !== 'production') {
 
 ReactDOM.render(
   <App />,
-  document.getElementById('root'),
+  document.getElementById('chat'),
 );
-
-// const p = document.createElement('p');
-// p.classList.add('card-text');
-// p.textContent = 'It works!';
-
-// const h5 = document.createElement('h5');
-// h5.classList.add('card-title');
-// h5.textContent = 'Project frontend l4 boilerplate';
-
-// const cardBody = document.createElement('div');
-// cardBody.classList.add('card-body');
-// cardBody.append(h5, p);
-
-// const card = document.createElement('div');
-// card.classList.add('card', 'text-center');
-// card.append(cardBody);
-
-// const container = document.querySelector('#chat');
-// container.append(card);
-
-// console.log('it works!');
