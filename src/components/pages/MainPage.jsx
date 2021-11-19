2bd5bdd7f8edf2492e96678ffc69b65874305f78
Aleksandr Chusovitin <chusovitinalexandr@gmail.com>
2021-11-19 11:49:35 +0700

    start dev MainPage

--- src/components/pages/MainPage.jsx
+++ src/components/pages/MainPage.jsx
@@ -1,5 +1,41 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-const MainPage = () => <h1>Main Page</h1>;
+import useAuth from '../../hooks/index.js';
+import routes from '../../routes.js';
+
+const getAuthHeader = (auth) => {
+  const user = JSON.parse(auth.getUserToken());
+
+  if (user && user.token) {
+    return { Authorization: `Bearer ${user.token}` };
+  }
+
+  return {};
+};
+
+const MainPage = () => {
+  const [chatChannels, setChatChannels] = useState([]);
+  const [chatMessages, setChatMessages] = useState([]);
+
+  const auth = useAuth();
+  const headers = getAuthHeader(auth);
+
+  useEffect(() => {
+    const fetchChannels = async () => {
+      const { data } = await axios.get(routes.usersPath(), { headers });
+      const { channels, messages } = data;
+
+      console.log(channels);
+      console.log(messages);
+      setChatChannels(data);
+      setChatMessages(messages);
+    };
+
+    fetchChannels();
+  }, []);
+
+  return <h1>MainPage</h1>;
+};
 
 export default MainPage;
